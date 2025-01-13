c66c132a02add4f4083da65346b631b39cacb4ef
ncor <toffuyume@gmail.com>
2025-01-13 14:04:29 +0300

    chore: move argument description to jsdoc

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,14 +2,11 @@
  * Creates a function that will invoke the provided function `fn` only once,
  * caching the result for subsequent calls with the same arguments.
  *
+ * @param fn - The function to invoke once and cache the result.
+ *
  * @returns A new function that returns the cached result after the first call.
  */
-export const once = <A extends any[], R>(
-    /**
-     * The function to invoke once and cache the result.
-     */
-    fn: (...args: A) => R,
-) => {
+export const once = <A extends any[], R>(fn: (...args: A) => R) => {
     let returned = false;
     let result: R | undefined;
 
